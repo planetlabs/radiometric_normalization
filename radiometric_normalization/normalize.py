5d2b9637643b64ae258dbd1c5b7852c4569f9eb4
Arin Jumpasut <arin.jumpasut@planet.com>
2015-03-18 10:05:58 +0000

    LUT replacement: Removed the LUT functions from normalize.py to clean it up

--- radiometric_normalization/normalize.py
+++ radiometric_normalization/normalize.py
@@ -52,38 +52,3 @@ def apply_transform(input_path, transformations, output_path):
     for i in range(len(img.bands)):
         img.bands[i] = apply_lut(img.bands[i], luts[i])
     img.save(output_path)
-
-
-def generate_luts(candidate_path, reference_paths, config=None):
-    if config is None:
-        config = {'time_stack_method': 'identity',
-                  'pif_method': 'identity',
-                  'transformation_method': 'linear_relationship'}
-
-    reference_image = time_stack.generate(
-        reference_paths,
-        method=config['time_stack_method'])
-
-    pif_weight, reference_img, candidate_img = pif.generate(
-        candidate_path, reference_path=reference_image,
-        method=config['pif_method'])
-    transformations = transformation.generate(
-        pif_weight, reference_img, candidate_img,
-        method=config['transformation_method'])
-    luts = transformation.transformations_to_luts(transformations)
-    return luts
-
-
-def apply_luts(input_path, luts, output_path):
-    def apply_lut(band, lut):
-        'Changes band intensity values based on intensity look up table (lut)'
-        if lut.dtype != band.dtype:
-            raise Exception(
-                "Band ({}) and lut ({}) must be the same data type.").format(
-                band.dtype, lut.dtype)
-        return numpy.take(lut, band, mode='clip')
-
-    img = gimage.load(input_path)
-    for i in range(len(img.bands)):
-        img.bands[i] = apply_lut(img.bands[i], luts[i])
-    img.save(output_path)
