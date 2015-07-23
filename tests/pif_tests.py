1a2d96afc958b260686b3ae753c190da5c256541
Arin Jumpasut <arin.jumpasut@planet.com>
2015-07-23 19:03:22 +0100

    Unit tests

--- tests/pif_tests.py
+++ tests/pif_tests.py
@@ -43,6 +43,112 @@ class Tests(unittest.TestCase):
         numpy.testing.assert_array_equal(test_pif_weight,
                                          golden_pif_weight)
 
+    def test__PCA_fit_single_band(self):
+        test_pca = pif._PCA_fit_single_band(numpy.array([1, 2, 3, 4, 5]),
+                                            numpy.array([1, 2, 3, 4, 5]))
+        sqrt_0_5 = numpy.sqrt(0.5)
+        numpy.testing.assert_array_almost_equal(
+            test_pca.components_, numpy.array([[sqrt_0_5, sqrt_0_5],
+                                               [-1 * sqrt_0_5, sqrt_0_5]]))
+        self.assertAlmostEqual(test_pca.explained_variance_[1], 0)
+        self.assertTrue(
+            test_pca.explained_variance_[0] > test_pca.explained_variance_[1])
+        test_pca = pif._PCA_fit_single_band([1.00001, 1, 1, 1, 1],
+                                            [0, 0, 0, 0, 0])
+        numpy.testing.assert_array_almost_equal(
+            test_pca.components_, numpy.array([[-1, 0],
+                                               [0, 1]]))
+
+    def test__PCA_filter_single_band(self):
+        test_pca = pif._PCA_fit_single_band([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
+        passed_pixels = pif._PCA_filter_single_band(
+            test_pca, [1.1, 2.5, 3, 10, 5], [1.1, 2.5, 2.5, 4, 5], 1)
+        numpy.testing.assert_array_equal(passed_pixels,
+                                         numpy.array([[0, 1, 2, 4]]))
+
+    def test__PCA_PIF_single_band(self):
+        golden_pif_weight = numpy.array(
+            [[1, 0, 0, 1, 1],
+             [0, 0, 0, 1, 1],
+             [1, 1, 1, 1, 1],
+             [0, 0, 0, 0, 0],
+             [1, 1, 0, 0, 0]])
+        array_shape = (5, 5)
+        valid_pixels = numpy.array([[0, 1, 2, 3, 4, 5, 6, 8, 9,
+                                     10, 11, 12, 13, 14, 15, 20, 21]])
+        passed_pixels = numpy.array([[0, 3, 4, 7, 8, 9, 10, 11,
+                                      12, 13, 15, 16]])
+        pif_weight = pif._PCA_PIF_single_band(passed_pixels, valid_pixels,
+                                              array_shape)
+        numpy.testing.assert_array_equal(pif_weight, golden_pif_weight)
+
+    def test__PCA_fit_and_filter_single_band(self):
+        ref_band = numpy.array([1, 2, 3,
+                                4, 5, 6,
+                                7, 8, 9])
+        cand_band = numpy.array([1.1, 1.9, 2.9,
+                                 10, 5, 7,
+                                 7.1, 7.9, 9])
+        alpha_vec = numpy.array([65535, 65535, 65535,
+                                 0, 65535, 65535,
+                                 65535, 65535, 65535])
+        valid_pixels = numpy.nonzero(alpha_vec != 0)
+        array_shape = (3, 3)
+
+        # Standard test
+        pif_weight = pif._PCA_fit_and_filter_single_band(cand_band, ref_band,
+                                                         valid_pixels,
+                                                         array_shape,
+                                                         0.5, None)
+        golden_pif_weight = numpy.array([[1, 1, 1],
+                                         [0, 1, 0],
+                                         [1, 1, 1]])
+        numpy.testing.assert_array_equal(pif_weight, golden_pif_weight)
+
+        # Make a very tight range
+        pif_weight = pif._PCA_fit_and_filter_single_band(cand_band, ref_band,
+                                                         valid_pixels,
+                                                         array_shape,
+                                                         0.001, None)
+        golden_pif_weight = numpy.array([[0, 0, 0],
+                                         [0, 0, 0],
+                                         [0, 0, 0]])
+        numpy.testing.assert_array_equal(pif_weight, golden_pif_weight)
+
+        # Make a very wide range
+        pif_weight = pif._PCA_fit_and_filter_single_band(cand_band, ref_band,
+                                                         valid_pixels,
+                                                         array_shape,
+                                                         1000, None)
+        golden_pif_weight = numpy.array([[1, 1, 1],
+                                         [0, 1, 1],
+                                         [1, 1, 1]])
+        numpy.testing.assert_array_equal(pif_weight, golden_pif_weight)
+
+        # Test batches small
+        pif_weight = pif._PCA_fit_and_filter_single_band(cand_band, ref_band,
+                                                         valid_pixels,
+                                                         array_shape,
+                                                         0.5, 1)
+        golden_pif_weight = numpy.array([[1, 1, 1],
+                                         [0, 1, 0],
+                                         [1, 1, 1]])
+        numpy.testing.assert_array_equal(pif_weight, golden_pif_weight)
+
+        # Test batches even
+        pif_weight = pif._PCA_fit_and_filter_single_band(cand_band, ref_band,
+                                                         valid_pixels,
+                                                         array_shape,
+                                                         0.5, 2)
+        numpy.testing.assert_array_equal(pif_weight, golden_pif_weight)
+
+        # Test batches large
+        pif_weight = pif._PCA_fit_and_filter_single_band(cand_band, ref_band,
+                                                         valid_pixels,
+                                                         array_shape,
+                                                         0.5, 10000)
+        numpy.testing.assert_array_equal(pif_weight, golden_pif_weight)
+
 
 if __name__ == '__main__':
     unittest.main()
