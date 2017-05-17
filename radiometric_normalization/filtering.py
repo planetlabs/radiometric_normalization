0b12109a946695dafb10d97cce33ed0edd977e81
Arin Jumpasut <arin.jumpasut@planet.com>
2017-05-17 09:03:49 -0700

    Refactoring the PIF code (implementing robust, implementing rough filters and making pca filtering a separate module)

---
+++ radiometric_normalization/filtering.py
@@ -0,0 +1,208 @@
+'''
+Copyright 2015 Planet Labs, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+import logging
+import numpy
+
+
+def _get_residual(data_1, data_2, line_gain, line_offset):
+    return numpy.absolute(line_gain * data_1 - data_2 + line_offset) / \
+        numpy.sqrt(1 + line_gain * line_gain)
+
+
+def combine_alphas(list_of_alphas):
+    return numpy.logical_and.reduce(list_of_alphas)
+
+
+def calculate_residuals_from_line(candidate_band, reference_band,
+                                  combined_alpha, line_gain=1.0,
+                                  line_offset=0.0):
+    ''' Calculates the residuals from a line.
+
+    :param array candidate_band: A 2D array representing the image data of the
+                                 candidate band
+    :param array reference_band: A 2D array representing the image data of the
+                                 reference image
+    :param array combined_alpha: A 2D array representing the alpha mask of the
+                                 valid pixels in both the candidate array and
+                                 reference array
+    :param float line_gain: The gradient of the line
+    :param float line_offset: The intercept of the line
+
+    :returns: A 2-D array of floats with the residuals of each valid pixel
+             (otherwise 0)
+    '''
+    logging.info('PIF Filtering: Calculating residuals from line: y = '
+                 '{} * x + {}'.format(line_gain, line_offset))
+
+    valid_pixels = numpy.nonzero(combined_alpha)
+
+    residuals = _get_residual(
+        candidate_band[valid_pixels], reference_band[valid_pixels],
+        line_gain, line_offset)
+
+    output = numpy.zeros(candidate_band.shape)
+    output[valid_pixels] = residuals
+    return output
+
+
+def filter_by_residuals_from_line(candidate_band, reference_band,
+                                  combined_alpha, threshold=1000,
+                                  line_gain=1.0, line_offset=0.0):
+    ''' Filters pixels by their residuals from a line
+
+    :param array candidate_band: A 2D array representing the image data of the
+                                 candidate band
+    :param array reference_band: A 2D array representing the image data of the
+                                 reference image
+    :param array combined_alpha: A 2D array representing the alpha mask of the
+                                 valid pixels in both the candidate array and
+                                 reference array
+    :param float threshold: The distance from the line within which to include
+                            pixels
+    :param float line_gain: The gradient of the line
+    :param float line_offset: The intercept of the line
+
+    :returns: A 2-D array of boolean mask representing each valid pixel (True)
+    '''
+    logging.info('PIF Filtering: Filtering from line: y = '
+                 '{} * x + {}'.format(line_gain, line_offset))
+
+    residual_band = calculate_residuals_from_line(
+        candidate_band, reference_band, combined_alpha, line_gain, line_offset)
+
+    mask = numpy.zeros(candidate_band.shape, dtype=numpy.bool)
+    mask[numpy.nonzero(residual_band < threshold)] = 1
+    return mask
+
+
+def filter_by_histogram(candidate_band, reference_band,
+                        combined_alpha, threshold=0.1,
+                        number_of_valid_bins=None, rough_search=False,
+                        number_of_total_bins_in_one_axis=10):
+    ''' Filters pixels using a 2D histogram of common values.
+
+    There is one mutually exclusive option:
+    - either a number_of_valid_bins is specified, which represents how many of
+      the most popular bins to select
+    - or a threshold is specified, which specifies a minimum population above
+      which a bin is selected
+
+    :param array candidate_band: A 2D array representing the image data of the
+                                 candidate band
+    :param array reference_band: A 2D array representing the image data of the
+                                 reference image
+    :param array combined_alpha: A 2D array representing the alpha mask of the
+                                 valid pixels in both the candidate array and
+                                 reference array
+    :param float threshold: A threshold on the population of a bin. Above this
+                            number, a bin is selected. This number is a
+                            fraction of the maximum bin (i.e. a value of 0.1
+                            will mean that the bin will need to have more than
+                            10% than the most populous bin)
+    :param int number_of_valid_bins: The total number of bins to select. The
+                                     bins are arranged in descending order of
+                                     population. This number specifies how
+                                     many of the top bins are selected (i.e.
+                                     a value of 3 will mean that the top three
+                                     bins are selected). If this is specified
+                                     then the threshold parameter is ignored
+    :param boolean rough_search: If this is true, the bins will specify a
+                                 maximum and minimum range within which to
+                                 select pixels (i.e. one superset bin will be
+                                 created from the bins that are selected).
+                                 This should speed up the computation but be
+                                 less exact
+    :param int number_of_total_bins_in_one_axis: This number controls the total
+                                                 number of bins. This number
+                                                 represents the total number of
+                                                 bins in one axis. Both axes
+                                                 have the same number (i.e. a
+                                                 value of 10 will mean that
+                                                 there are 100 bins in total)
+
+    :returns: A 2-D array of boolean mask representing each valid pixel (True)
+    '''
+    logging.info('PIF Filtering: Filtering by histogram.')
+
+    valid_pixels = numpy.nonzero(combined_alpha)
+    candidate_data = candidate_band[valid_pixels]
+    reference_data = reference_band[valid_pixels]
+
+    H, candidate_bins, reference_bins = numpy.histogram2d(
+        candidate_data, reference_data, bins=number_of_total_bins_in_one_axis)
+
+    def check_in_valid_bin(c_data_point, r_data_point):
+        for valid_cand_bin, valid_ref_bin in zip(
+          passed_bins[0], passed_bins[1]):
+            if c_data_point >= candidate_bins[valid_cand_bin] and \
+               c_data_point <= candidate_bins[valid_cand_bin + 1] and \
+               r_data_point >= reference_bins[valid_ref_bin] and \
+               r_data_point <= reference_bins[valid_ref_bin + 1]:
+                return True
+        return False
+
+    def get_valid_range():
+        c_min = min([candidate_bins[v] for v in passed_bins[0]])
+        c_max = min([candidate_bins[v + 1] for v in passed_bins[0]])
+        r_min = min([reference_bins[v] for v in passed_bins[1]])
+        r_max = min([reference_bins[v + 1] for v in passed_bins[1]])
+        return c_min, c_max, r_min, r_max
+
+    def check_in_valid_range(c_data_point, r_data_point):
+        if c_data_point >= c_min and \
+           c_data_point <= c_max and \
+           r_data_point >= r_min and \
+           r_data_point <= r_max:
+            return True
+        return False
+
+    if number_of_valid_bins:
+        logging.info('Filtering by number of histogram bins.')
+        passed_bins = numpy.unravel_index(
+          numpy.argsort(H.ravel())[-number_of_valid_bins:], H.shape)
+    else:
+        logging.info('Filtering by threshold.')
+        H_max = float(max(H.flatten()))
+        passed_bins = numpy.nonzero(H / H_max > threshold)
+        logging.info(
+            '{} bins out of {} passed'.format(
+                len(passed_bins[0]), len(H.flatten())))
+
+    if rough_search:
+        logging.info('Rough filtering only')
+        c_min, c_max, r_min, r_max = get_valid_range()
+        logging.info(
+            'Valid range: Candidate = ({}, {}), Reference = ({}, {})'.format(
+                c_min, c_max, r_min, r_max))
+        passed_pixels = numpy.nonzero(
+            [check_in_valid_range(c, r) for c, r in zip(
+              candidate_data, reference_data)])
+    else:
+        logging.info('Exact filtering by bins')
+        passed_pixels = numpy.nonzero(
+            [check_in_valid_bin(c, r) for c, r in zip(
+                candidate_data, reference_data)])
+    logging.info(
+        'Valid data = {} out of {} ({}%)'.format(
+            len(passed_pixels[0]),
+            len(valid_pixels[0]),
+            100.0 * len(passed_pixels[0]) / len(valid_pixels[0])))
+
+    mask_pixels = (valid_pixels[0][passed_pixels[0]],
+                   valid_pixels[1][passed_pixels[0]])
+    mask = numpy.zeros(candidate_band.shape, dtype=numpy.bool)
+    mask[mask_pixels] = 1
+    return mask
--- radiometric_normalization/pif.py
+++ radiometric_normalization/pif.py
@@ -13,196 +13,141 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-import itertools
 import logging
 import numpy
 from collections import namedtuple
-from sklearn.decomposition import PCA
+
+from radiometric_normalization import pca_filter
+from radiometric_normalization import robust
+from radiometric_normalization import filtering
 
 
-pca_options = namedtuple('pca_options', 'limit')
-DEFAULT_PCA_OPTIONS = pca_options(limit=30)
+pca_options = namedtuple('pca_options', 'threshold')
+DEFAULT_PCA_OPTIONS = pca_options(threshold=30)
+
+robust_options = namedtuple('robust_options', 'threshold')
+DEFAULT_ROBUST_OPTIONS = robust_options(threshold=1000)
 
 
-def generate_alpha_band_pifs(candidate_alpha, reference_alpha):
+def generate_alpha_band_pifs(combined_alpha):
     ''' Creates the pseudo-invariant features from the reference and candidate
     alpha masks (filtering out pixels where either the candidate or reference
     is masked, i.e. the alpha value is False).
 
-    :param array candidate_alpha: A 2D array representing the alpha mask of the
-                                  candidate image
-    :param array reference_alpha: A 2D array representing the alpha mask of the
-                                  reference image
+    :param array combined_alpha: A 2D array representing the alpha mask of the
+                                 valid pixels in both the candidate array and
+                                 reference array
 
     :returns: A 2-D boolean array representing pseudo invariant features
     '''
     logging.info('Pseudo invariant feature generation is using: Filtering '
                  'using the alpha mask.')
-    combined_alpha = numpy.logical_and(candidate_alpha, reference_alpha)
+
+    # Only analyse valid pixels
     valid_pixels = numpy.nonzero(combined_alpha)
 
-    if logging.getLogger().getEffectiveLevel() <= logging.INFO:
-        no_total_pixels = candidate_alpha.size
-        no_valid_pixels = len(valid_pixels[0])
-        valid_percent = 100.0 * no_valid_pixels / no_total_pixels
-        logging.info(
-            'PCA Info: Found {} final pifs out of {} pixels ({}%)'.format(
-                no_valid_pixels, no_total_pixels, valid_percent))
-
-    pif_mask = numpy.zeros(candidate_alpha.shape, dtype=numpy.bool)
+    pif_mask = numpy.zeros(combined_alpha.shape, dtype=numpy.bool)
     pif_mask[valid_pixels] = True
+
+    if logging.getLogger().getEffectiveLevel() <= logging.INFO:
+        pif_pixels = numpy.nonzero(pif_mask)
+        no_pif_pixels = len(pif_pixels[0])
+        no_total_pixels = combined_alpha.size
+        valid_percent = 100.0 * no_pif_pixels / no_total_pixels
+        logging.info(
+            'PIF Info: Found {} final PIFs out of {} pixels ({}%)'.format(
+                no_pif_pixels, no_total_pixels, valid_percent))
 
     return pif_mask
 
 
-def generate_pca_pifs(candidate_band, candidate_alpha,
-                      reference_band, reference_alpha,
+def generate_robust_pifs(candidate_band, reference_band, combined_alpha,
+                         parameters=DEFAULT_ROBUST_OPTIONS):
+    ''' Performs a robust fit to the valid pixels and filters according
+    to the distance from the fit line.
+
+    :param array candidate_band: A 2D array representing the image data of the
+                                 candidate band
+    :param array reference_band: A 2D array representing the image data of the
+                                 reference image
+    :param array combined_alpha: A 2D array representing the alpha mask of the
+                                 valid pixels in both the candidate array and
+                                 reference array
+    :param robust_options parameters: Method specific parameters. Currently:
+        threshold (float): Representing the distance from the fit line
+                           to look for PIF pixels
+
+    :returns: A 2-D boolean array representing pseudo invariant features
+    '''
+    logging.info('Pseudo invariant feature generation is using: Filtering '
+                 'using a robust fit.')
+
+    # Only analyse valid pixels
+    valid_pixels = numpy.nonzero(combined_alpha)
+
+    # Robust fit
+    gain, offset = robust.fit(
+        candidate_band[valid_pixels], reference_band[valid_pixels])
+
+    # Filter using the robust fit
+    pif_mask = filtering.filter_by_residuals_from_line(
+        candidate_band, reference_band, combined_alpha,
+        threshold=parameters.threshold, line_gain=gain, line_offset=offset)
+
+    if logging.getLogger().getEffectiveLevel() <= logging.INFO:
+        _info_logging(candidate_band, reference_band,
+                     valid_pixels, numpy.nonzero(pif_mask))
+
+    return pif_mask
+
+
+def generate_pca_pifs(candidate_band, reference_band, combined_alpha,
                       parameters=DEFAULT_PCA_OPTIONS):
     ''' Performs PCA analysis on the valid pixels and filters according
     to the distance from the principle eigenvector.
 
     :param array candidate_band: A 2D array representing the image data of the
                                  candidate band
-    :param array candidate_alpha: A 2D array representing the alpha mask of the
-                                  candidate image
     :param array reference_band: A 2D array representing the image data of the
-                                  reference image
-    :param array reference_alpha: A 2D array representing the alpha mask of the
-                                  reference image
+                                 reference image
+    :param array combined_alpha: A 2D array representing the alpha mask of the
+                                 valid pixels in both the candidate array and
+                                 reference array
     :param pca_options parameters: Method specific parameters. Currently:
-        limit (float): Representing the width of the PCA filter
+        threshold (float): Representing the width of the PCA filter
 
     :returns: A 2-D boolean array representing pseudo invariant features
     '''
     logging.info('Pseudo invariant feature generation is using: Filtering '
                  'using PCA.')
 
-    # Only analyse valid pixels
-    combined_alpha = numpy.logical_and(candidate_alpha, reference_alpha)
-    valid_pixels = numpy.nonzero(combined_alpha)
-
     # Find PIFs
-    passed_pixels = _pca_fit_and_filter_valid_pixels(
-        candidate_band[valid_pixels], reference_band[valid_pixels],
-        parameters)
-    pif_mask = _create_pif_mask(passed_pixels, combined_alpha)
+    pif_mask = pca_filter.get_pif_mask(
+        candidate_band, reference_band, combined_alpha, parameters)
 
     if logging.getLogger().getEffectiveLevel() <= logging.INFO:
-        _pca_logging(candidate_band, reference_band,
-                     valid_pixels, numpy.nonzero(pif_mask))
+        _info_logging(candidate_band, reference_band,
+                      numpy.nonzero(combined_alpha),
+                      numpy.nonzero(pif_mask))
 
     return pif_mask
 
 
-def _pca_logging(c_band, r_band, valid_pixels, pif_pixels):
+def _info_logging(c_band, r_band, valid_pixels, pif_pixels):
     ''' Optional logging information
     '''
-    logging.info('PCA Info: Original corrcoef = {}'.format(
+    logging.info('PIF Info: Original corrcoef = {}'.format(
         numpy.corrcoef(c_band[valid_pixels], r_band[valid_pixels])[0, 1]))
 
     if pif_pixels[0] != [] and pif_pixels[1] != []:
-        logging.info('PCA Info: Filtered corrcoef = {}'.format(
+        logging.info('PIF Info: Filtered corrcoef = {}'.format(
             numpy.corrcoef(c_band[pif_pixels], r_band[pif_pixels])[0, 1]))
 
         no_pif_pixels = len(pif_pixels[0])
         no_total_pixels = c_band.size
         valid_percent = 100.0 * no_pif_pixels / no_total_pixels
         logging.info(
-            'PCA Info: Found {} final PIFs out of {} pixels ({}%)'.format(
+            'PIF Info: Found {} final PIFs out of {} pixels ({}%)'.format(
                 no_pif_pixels, no_total_pixels, valid_percent))
     else:
-        logging.info('PCA Info: No PIF pixels found.')
-
-
-def _pca_fit_and_filter_valid_pixels(candidate_pixels, reference_pixels,
-                                     parameters):
-    ''' Performs PCA analysis, on the valid pixels and filters according
-    to the distance from the principle eigenvector, for a single band.
-    '''
-    fitted_pca = _pca_fit_single_band(candidate_pixels, reference_pixels)
-    passed_pixels = _pca_filter_single_band(
-        fitted_pca, candidate_pixels, reference_pixels, parameters.limit)
-
-    return passed_pixels
-
-
-def _pca_fit_single_band(cand_valid, ref_valid):
-    ''' Uses SK Learn PCA module to do PCA fit
-    '''
-
-    X = _numpy_array_from_2arrays(cand_valid, ref_valid)
-
-    # SK Learn PCA
-    pca = PCA(n_components=2)
-
-    # Fit the points
-    pca.fit(X)
-
-    return pca
-
-
-def _numpy_array_from_2arrays(array1, array2, dtype=numpy.uint16):
-    '''Efficiently combine two 1-D arrays into a single 2-D array.
-
-    Avoids large memory usage by creating the array using
-    ``numpy.fromiter`` and then reshaping a view of the resulting
-    record array.  This does the equivalent of:
-
-        numpy.array(zip(array1, array2))
-
-    but avoids holding a potentially large number of tuples in memory.
-
-    >>> a = numpy.array([1, 2, 3])
-    >>> b = numpy.array([4, 5, 6])
-    >>> X = _numpy_array_from_2arrays(a, b)
-    >>> X
-    array([[1, 4],
-           [2, 5],
-           [3, 6]], dtype=uint16)
-
-    :param array array1: A 1-D numpy array.
-    :param array array2: A second 1-D numpy array.
-    :param data-type dtype: Data type for array elements
-        (must be same for both arrays)
-
-    :returns: A 2-D numpy array combining the two input arrays
-    '''
-
-    array_dtype = [('x', dtype), ('y', dtype)]
-
-    return numpy.fromiter(itertools.izip(array1, array2), dtype=array_dtype) \
-                .view(dtype=dtype) \
-                .reshape((-1, 2))
-
-
-def _pca_filter_single_band(pca, cand_valid, ref_valid, limit):
-    ''' Uses SciKit Learn PCA module to transform the data and filter
-    '''
-
-    X = _numpy_array_from_2arrays(cand_valid, ref_valid)
-    X_trans = pca.transform(X)
-
-    # This is ok, only a single tuple and a 1-D array is being created
-    _, ref_valid_trans_list = zip(*X_trans)
-    ref_valid_trans = numpy.array(ref_valid_trans_list)
-
-    # Filter
-    pixels_pass_filter = numpy.nonzero(numpy.logical_and(
-        ref_valid_trans >= (limit * -1), ref_valid_trans <= limit))[0]
-
-    return pixels_pass_filter
-
-
-def _create_pif_mask(passed_pixels, alpha):
-    ''' Converts the list of filtered pixels to a PIF array
-    '''
-
-    # PIF images
-    pif = numpy.zeros(alpha.shape, dtype=numpy.bool)
-    pif_vec = pif.ravel()
-    alpha_vec = alpha.ravel()
-    valid_pixels = numpy.nonzero(alpha_vec)[0]
-    pif_vec[valid_pixels[passed_pixels]] = 1
-    pif = numpy.reshape(pif_vec, alpha.shape)
-
-    return pif
+        logging.info('PIF Info: No PIF pixels found.')
