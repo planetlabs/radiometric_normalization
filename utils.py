3c0693fd02bd05ebc823907c7337b03754121dde
Sagnik Ghosh <ee19b132@smail.iitm.ac.in>
2021-07-20 17:01:49 +0530

    New branch

--- utils.py
+++ utils.py
@@ -26,7 +26,11 @@ def compute_score(kernel_filepath):
     
     return score
 
+<<<<<<< HEAD
 def perform_data_process(image_path, ref_image_path=None,out_directory=None, out_path=None, deblur=False, product='TOA', source = 'PLANET'):
+=======
+def perform_data_process_write(image_path, ref_image_path=None,out_directory=None, out_path=None, deblur=False, product='TOA', source = 'PLANET'):
+>>>>>>> 19b7f46 (New branch)
     """
     Definition: Function to perform normalization and deblurring of image_path wrt referenced image
     image_path: Image to be processed
@@ -38,14 +42,12 @@ def perform_data_process(image_path, ref_image_path=None,out_directory=None, out
     try:
         temp_folder = os.path.join(os.path.dirname(__file__), "temp")
         kernel_folder = os.path.join(os.path.dirname(__file__), "kernel")
-        region_reference_folder = os.path.dirname(__file__)
     except:
         temp_folder = os.path.join(".","temp")
         kernel_folder = os.path.join(".", "kernel")
-        region_reference_folder = "."
     os.makedirs(temp_folder, exist_ok=True)
     os.makedirs(kernel_folder, exist_ok=True)
-    region_reference_file_path =  os.path.join(region_reference_folder, 'region_reference.json')
+    region_reference_file_path =  'region_reference.json'
     image_name = Path(image_path).stem
     extension = Path(image_path).suffix
     region = image_name.split('_')[0]
